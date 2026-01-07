bbe77da1113fd2855f1adc9b49ed239428072622
chinmayawasthi <94186244+Achinmay05@users.noreply.github.com>
2026-01-08 01:48:45 +0530

    ...

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,16 @@
+// import { defineConfig } from "tailwindcss";
+
+// export default defineConfig({
+//   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#5f6FFF",
+//       },
+//       gridTemplateColumns: {
+//         auto: "repeat(auto-fill, minmax(200px, 1fr))",
+//       },
+//     },
+//   },
+//   safelist: ["bg-primary", "grid-cols-auto"], // ensures v4 generates them
+// });
